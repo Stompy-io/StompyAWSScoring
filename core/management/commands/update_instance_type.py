9f8a2c93657e9fb1ba29b097f76f552b16c70bb3
sophiawang1228 <wyuean@stompy.io>
2021-07-16 18:10:52 +0800

    finished storing aws data in mongo

--- core/management/commands/update_instance_type.py
+++ core/management/commands/update_instance_type.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from core.spot_market_scoring.mappings import REGION_CODE_MAP
 import boto3
 import time
 
@@ -8,7 +9,8 @@ class Command(BaseCommand):
     
     def save_data_to_mongo(self, region_code, data):
         dbclient = MongoClient(settings.MONGODB_CONNECTION)
-        db = dbclient['aws_data']['instance_type']
+        db = dbclient['aws-data']
+        collection = db['instance_type']
         data_filter = {'region_code': region_code}
         update_data = {
             '$setOnInsert': {
@@ -16,14 +18,29 @@ class Command(BaseCommand):
                 'data': data
             }
         }
-        db.updateMany(data_filter, update_data, upsert=True)
+        data = {
+            'region_code': region_code,
+            'data': data
+        }
+        # collection.update_many(data_filter, update_data, upsert=True)
+        collection.insert_one(data)
     
     def get_aws_data(self):
         pass        
 
     def handle(self, *args, **options):
         start = time.time()
-        session = boto3.session.Session(region_name='us-east-1', **settings.AWS_CREDENTIALS)
-        ec2_client = session.client('ec2')
-        print(ec2_client.describe_instance_types())
+        for region in REGION_CODE_MAP.keys():
+            session = boto3.session.Session(region_name=region, **settings.AWS_CREDENTIALS)
+            ec2_client = session.client('ec2')
+
+            response = ec2_client.describe_instance_types()
+            results = []
+            results.extend(response['InstanceTypes'])
+            while 'NextToken' in response:
+                response = ec2_client.describe_instance_types(NextToken=response['NextToken'])
+                results.extend(response['InstanceTypes'])
+
+            self.save_data_to_mongo(region_code=region,data=results)
+        # print(results)
         print(time.time() - start)
