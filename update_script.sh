8b104177d60df48b9b1b07e7be423777e29676be
Neo-Wong <843327849@qq.com>
2021-07-26 15:33:46 +0800

    Remove the config file our of project

--- update_script.sh
+++ update_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source /home/neo/StompyAWSScoring/ev.txt
+source /home/neo/aws_scoring_config.txt
 
 nohup /home/neo/.virtualenvs/StompyAWSScoring/bin/python /home/neo/StompyAWSScoring/update_spot_market_scores.py >> /home/neo/StompyAWSScoring/log 2>&1 &
